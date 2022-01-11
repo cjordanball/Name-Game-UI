194d8e01ed27b1c0a52754ef6274438de95788d8
Jordan Ball <jordan.ball@willowtreeapps.com>
2022-01-11 13:57:44 -0500

    initial setup and fetching of data

---
+++ NameGameUI/Models/NameGameModel.swift
@@ -0,0 +1,12 @@
+//
+//  NameGameModel.swift
+//  NameGameUI
+//
+//  Created by Jordan Ball on 1/11/22.
+//
+
+import Foundation
+
+struct NameGameModel {
+    
+}
---
+++ NameGameUI/ReferenceValues.swift
@@ -0,0 +1,13 @@
+//
+//  ReferenceValues.swift
+//  NameGameUI
+//
+//  Created by Jordan Ball on 1/11/22.
+//
+
+import Foundation
+
+struct ReferenceValues {
+    let peopleDataString: String = "https://namegame.willowtreeapps.com/api/v1.0/profiles"
+
+}
